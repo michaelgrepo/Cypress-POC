1ab019c7d116350cf2dd80f21e84a29bd61b2458
Michael <michaelgilbert1@gmail.com>
2020-10-27 09:52:00 +0200

    First Commit

---
+++ cypress/integration/poctests.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+
+  describe('First Test',()=> {
+    it('loads page',()=>{
+    cy.visit("https://dev-online.dev.tsretail.co.za/sport?site=gauteng")
+        })
+
+   })
